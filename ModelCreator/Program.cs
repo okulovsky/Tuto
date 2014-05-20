8f9cd61bf79d8ab3576efeaac0ef39883cff900c
okulovsky <yuri.okulovsky@gmail.com>
2014-05-20 13:48:49 +0600

    Folders of the solution are renamed

--- ModelCreator/Program.cs
+++ ModelCreator/Program.cs
@@ -1,10 +1,10 @@
-﻿using System;
+﻿using Editor;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Editor;
 
 namespace ModelCreator
 {
