037ee0c3bac269f7976d80ede6af53c99f9daaea
okulovsky <yuri.okulovsky@gmail.com>
2014-09-29 13:28:54 +0600

    + Utilities project

---
+++ Utilities/Statistics.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utilities
+{
+    class Statistics
+    {
+        public void Run()
+        {
+            var allData = Tuto.Model.EditorModelIO.ReadAllProjectData(
+                new System.IO.DirectoryInfo("D:\\BP"));
+            var percents = new List<double>();
+            foreach (var e in allData.Models)
+            {
+                var total=0;
+                var good=0;
+                foreach (var c in e.Montage.Chunks)
+                {
+                    if (c.Mode == Editor.Mode.Undefined) continue;
+                    total += c.Length;
+                    if (c.Mode == Editor.Mode.Drop) continue;
+                    good += c.Length;
+                }
+                percents.Add((100.0 * good) / total);
+            }
+            Console.WriteLine(percents.OrderBy(z => z).Skip(10).Average());
+
+        }
+    }
+}
