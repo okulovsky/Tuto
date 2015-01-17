1d8545091e703e0e69c059b815c539007c4f7862
okulovsky <yuri.okulovsky@gmail.com>
2015-01-18 00:23:29 +0500

    Есть еще мелкие проблемы с Description у видео, плюс ключевые слова тоже не делаются

--- Tuto/Model/Current/Global/TopicLevel.cs
+++ Tuto/Model/Current/Global/TopicLevel.cs
@@ -1,14 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Tuto.Model
 {
+    [DataContract]
     public class TopicLevel
     {
+        [DataMember]
         public string Caption { get; set; }
+        [DataMember]
         public int Digits { get; set; }
     }
 }
