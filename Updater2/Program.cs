eeb2a72209b874bfef655bf1e0279bba8ec6812d
okulovsky <yuri.okulovsky@gmail.com>
2015-11-11 20:21:24 +0500

    + calculating hashes for videofiles

---
+++ Updater2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tuto;
+
+namespace Updater2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			return;
+			var path = "D:\\Misc";
+			var inputPath = path+"\\Input";
+			var dictionary = new Dictionary<DirectoryInfo,string>();
+			Library.MakeHashes(
+				new System.IO.DirectoryInfo(path),
+				"face.mp4",
+				"hash.txt",
+				true,
+				dictionary
+				);
+
+		}
+	}
+}
