a3379b24500ebdcf790928e184791580512770ef
okulovsky <yuri.okulovsky@gmail.com>
2015-11-29 14:07:01 +0500

    + новый обработчик звука

--- TestProject/Classes.cs
+++ TestProject/Classes.cs
@@ -1,24 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Windows;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
 
 namespace TestProject
 {
     class Program
     {
+
+        const double samplesLength = 0.25;
+        const double silenceTime = 3;
         public static void Main()
         {
-            string test =
-@"  {
-		//#video CRQnC44OO_A
+            var numberPattern = @"\d\.eE\-\+";
+            var regex = new Regex(@"([" + numberPattern + "]+)[ ]+([" + numberPattern + "]+)");
+            var result = new List<Tuple<double, double>>();
+            double sumAmplitude = 0;
+            double startTime = 0;
+            var sampleStarts = true;
+            
+            foreach(var line in File.ReadLines(@"C:\Users\Yura\Desktop\TestMontage\Input\Hackerdom\09\09-2\voice.dat").Skip(2))
+            {
+                var match = regex.Match(line);
+                if (!match.Success) 
+                    continue;
+                var time=double.Parse(match.Groups[1].Value,CultureInfo.InvariantCulture);
+                var amplitute = double.Parse(match.Groups[2].Value,CultureInfo.InvariantCulture);
+                if (sampleStarts)
+                {
+                    startTime = time;
+                    sampleStarts = false;
+                }
+                sumAmplitude += Math.Abs(amplitute);
+                if (time-startTime>samplesLength)
+                {
+                    result.Add(Tuple.Create(startTime, sumAmplitude / (time - startTime)));
+                    sampleStarts = true;
+                    sumAmplitude = 0;
+                    if (startTime > 60) break;
+                }
+            }
 
-        }";
-            var regexp = new Regex("//#video ([a-zA-Z0-9_]+)");
-            Console.WriteLine(regexp.Replace(test, "//#video mystrings"));
+            var silenceLevel = result.Where(z => z.Item1 < silenceTime).Max(z => z.Item2);
+       
+            var max = result.Max(z => z.Item2);
 
+            var chart = new Chart();
+            chart.ChartAreas.Add(new ChartArea());
+            var series = new Series();
+            var voice = new Series { ChartType = SeriesChartType.FastLine };
+            foreach (var e in result)
+            {
+                series.Points.AddXY(e.Item1, e.Item2);
+                voice.Points.AddXY(e.Item1, e.Item2 > silenceLevel ? max : 0);
+            }
+            chart.Series.Add(series);
+            chart.Series.Add(voice);
+            chart.Dock = System.Windows.Forms.DockStyle.Fill;
+            var form = new Form();
+            form.Controls.Add(chart);
+            System.Windows.Forms.Application.Run(form);
         }
     }
 }
--- Tuto/BatchWorks/PraatWork.cs
+++ Tuto/BatchWorks/PraatWork.cs
@@ -4,6 +4,7 @@ using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Input;
 using Tuto.Model;
@@ -12,87 +13,85 @@ namespace Tuto.BatchWorks
 {
     public class PraatWork : BatchWork
     {
+
+        const double samplesLength = 0.25;
+        const double silenceTime = 3;
+
+        FileInfo wav;
+        FileInfo dat;
+
         public PraatWork(EditorModel model)
         {
             Model = model;
             Name = "Praat working: " + model.Locations.FaceVideo.FullName;
+            wav = new FileInfo(Path.Combine(Model.TempFolder.FullName, "voice.wav"));
+            dat = new FileInfo(Path.Combine(Model.TempFolder.FullName, "voice.dat"));
         }
-
-        const string SilentLabel = "--";
-        const string SoundLabel = "++";
-
-        const double MinPitch = 100;
-        const double TimeStep = 0;
-
-        const double SilenceThreshold = -27;
-        const double MinSilentInterval = 0.4;
-        const double MinSoundInterval = 0.1;
 
         public event EventHandler PraatCreated;
 
         public override void Work()
         {
-            Model.Locations.PraatOutput.Delete();
-
-            var convert = "";
             var ffmpegExecutable = Model.Videotheque.Locations.FFmpegExecutable;
             var args = string.Format("-i \"{0}\" -vn -q:a 0 \"{1}\"", Model.Locations.FaceVideo, Model.Locations.PraatVoice);
             RunProcess(args, ffmpegExecutable.ToString());
-            var work = 
-                String.Format(
-                    CultureInfo.InvariantCulture,
-                    " --run \"{0}\" \"{1}\" \"{2}\" {3} {4} {5} {6} {7} {8} {9}",
-                    Model.Locations.PraatScriptSource,
-                    Model.Locations.PraatVoice,
-                    Model.Locations.PraatOutput,
-                    SilentLabel,
-                    SoundLabel,
-                    MinPitch,
-                    TimeStep,
-                    SilenceThreshold,
-                    MinSilentInterval,
-                    MinSoundInterval);
-            RunProcess(work, Model.Videotheque.Locations.PraatExecutable.ToString());
-            Model.Montage.SoundIntervals.Clear();
-            using (var reader = new StreamReader(Model.Locations.PraatOutput.FullName))
+
+            args = string.Format(@"-i ""{0}"" ""{1}""",Model.Locations.PraatVoice.FullName,wav);
+            RunProcess(args,ffmpegExecutable.ToString());
+
+            var numberPattern = @"\d\.eE\-\+";
+            var regex = new Regex(@"([" + numberPattern + "]+)[ ]+([" + numberPattern + "]+)");
+            var result = new List<Tuple<double, double>>();
+            double sumAmplitude = 0;
+            double startTime = 0;
+            var sampleStarts = true;
+            
+            foreach(var line in File.ReadLines(dat.FullName).Skip(2))
             {
-
-                for (var i = 0; i < 11; i++)
-                    reader.ReadLine();
-
-                var intervalCount = int.Parse(reader.ReadLine());
-                for (int i = 0; i < intervalCount; i++)
+                var match = regex.Match(line);
+                if (!match.Success) 
+                    continue;
+                var time=double.Parse(match.Groups[1].Value,CultureInfo.InvariantCulture);
+                var amplitute = double.Parse(match.Groups[2].Value,CultureInfo.InvariantCulture);
+                if (sampleStarts)
                 {
-                    var startTime = double.Parse(reader.ReadLine(), CultureInfo.InvariantCulture);
-                    var endTime = double.Parse(reader.ReadLine(), CultureInfo.InvariantCulture);
-                    var hasVoice = reader.ReadLine() == '"' + SoundLabel + '"';
-                    Model.Montage.SoundIntervals.Add(
-                        new SoundInterval(
-                            (int)Math.Round(startTime * 1000),
-                            (int)Math.Round(1000 * endTime),
-                            hasVoice));
+                    startTime = time;
+                    sampleStarts = false;
+                }
+                sumAmplitude += Math.Abs(amplitute);
+                if (time-startTime>samplesLength)
+                {
+                    result.Add(Tuple.Create(startTime, sumAmplitude / (time - startTime)));
+                    sampleStarts = true;
+                    sumAmplitude = 0;
+                    if (startTime > 60) break;
                 }
             }
 
+            var silenceLevel = result.Where(z => z.Item1 < silenceTime).Max(z => z.Item2);
+            var max = result.Max(z => z.Item2);
 
-            //  model.Locations.PraatVoice.Delete();
-            Model.Locations.PraatOutput.Delete();
-            OnTaskFinished();
+            var output = result.Select(z=>new SoundInterval
+            {
+                StartTime=(int)(z.Item1*1000),
+                EndTime=(int)(1000*(z.Item1+samplesLength)),
+                HasVoice=z.Item2>silenceLevel,
+                Volume=z.Item2/max
+            }).ToList();
+               
+            Model.Montage.SoundIntervals=output;
+            wav.Delete();
+            dat.Delete();
         }
+
+
 
         public override void Clean()
         {
             if (Process != null && !Process.HasExited)
                 Process.Kill();
-            if (File.Exists(Model.Locations.PraatOutput.FullName))
-            {
-                while (File.Exists(Model.Locations.PraatOutput.FullName))
-                    try
-                    {
-                        File.Delete(Model.Locations.PraatOutput.FullName);
-                    }
-                    catch { }
-            }
+            TryToDelete(wav);
+            TryToDelete(dat);
         }
     }
 }
