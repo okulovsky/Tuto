22c624a1f2cc6acd76bf3148d0fbbe5829b70351
okulovsky <yuri.okulovsky@gmail.com>
2015-12-29 20:03:23 +0500

    + полнотектовый поиск по моделям

---
+++ Tuto.Navigator/ViewModels/SearchViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tuto.Navigator.ViewModels
+{
+    public class SearchViewModel : NotifierModel
+    {
+        string textSearch;
+        public string TextSearch
+        {
+            get { return textSearch; }
+            set { 
+                textSearch = value; 
+                NotifyPropertyChanged(); }
+        }
+    }
+}
