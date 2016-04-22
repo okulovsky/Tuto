a878550ddf7167f31bacd92bd00cc1b08eb4e282
IvanSorokin <iwan954@mail.ru>
2016-04-22 21:04:37 +0600

    background converter added

--- Tuto/BatchWorks/Converters.cs
+++ Tuto/BatchWorks/Converters.cs
@@ -8,7 +8,7 @@ using System.Windows.Data;
 
 namespace Tuto.BatchWorks
 {
-    public class StatusConverter : IValueConverter
+    public class StatusConverterForeground : IValueConverter
     {
         //for foregrounds
         public Dictionary<BatchWorkStatus, string> colors = new Dictionary<BatchWorkStatus, string>()
@@ -20,6 +20,30 @@ namespace Tuto.BatchWorks
             {BatchWorkStatus.Failure, "Red" },
             {BatchWorkStatus.Success, "LightGreen" },
             {BatchWorkStatus.Attention, "Yellow" }
+        };
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return colors[(BatchWorkStatus)value];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value;
+        }
+    }
+    public class StatusConverterBackGround : IValueConverter
+    {
+        //for backgrounds
+        public Dictionary<BatchWorkStatus, string> colors = new Dictionary<BatchWorkStatus, string>()
+        {
+            {BatchWorkStatus.Running, "LightGray" },
+            {BatchWorkStatus.Aborted, "Red" },
+            {BatchWorkStatus.Cancelled, "DarkGray" },
+            {BatchWorkStatus.Pending, "LightGray" },
+            {BatchWorkStatus.Failure, "Red" },
+            {BatchWorkStatus.Success, "LightGray" },
+            {BatchWorkStatus.Attention, "LightGray" }
         };
 
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
